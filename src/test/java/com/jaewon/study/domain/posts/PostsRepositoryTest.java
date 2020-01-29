b0aa1fc77d0e329cff80afc8d933f8da8c0ba4af
jwsung <ssaekjae@naver.com>
2020-01-29 18:23:47 +0900

    add JPA, Posts Insert and Test

---
+++ src/test/java/com/jaewon/study/domain/posts/PostsRepositoryTest.java
@@ -0,0 +1,43 @@
+package com.jaewon.study.domain.posts;
+
+import org.junit.After;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PostsRepositoryTest {
+
+    @Autowired
+    PostsRepository postsRepository;
+
+    @After
+    public void cleanup() {
+        postsRepository.deleteAll();
+    }
+
+    @Test
+    public void saveAndFind() {
+        String title = "test";
+        String content = "content";
+
+        postsRepository.save(Posts.builder()
+                .title(title)
+                .content(content)
+                .author("jwsung")
+                .build());
+
+        List<Posts> postsList = postsRepository.findAll();
+
+        Posts posts = postsList.get(0);
+        assertThat(posts.getTitle()).isEqualTo(title);
+        assertThat(posts.getContent()).isEqualTo(content);
+    }
+}
