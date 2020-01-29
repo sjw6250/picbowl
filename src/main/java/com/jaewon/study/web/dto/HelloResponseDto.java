5f0e16aff8749a3f094b790e6fccc7545577e43d
jwsung <ssaekjae@naver.com>
2020-01-29 15:14:12 +0900

    downgrade gradle version, add dto test code, add lombok to gradle

---
+++ src/main/java/com/jaewon/study/web/dto/HelloResponseDto.java
@@ -0,0 +1,11 @@
+package com.jaewon.study.web.dto;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class HelloResponseDto {
+    private final String name;
+    private final int amount;
+}
